04f31a9a22c7d94369791a374e1f47dafd4d4ef0
Dave Defourneaux <74273226+dd4no@users.noreply.github.com>
2021-07-30 10:14:54 -0400

    Update game.js
    basic card deal

--- game.js
+++ game.js
@@ -15,5 +15,24 @@
   }
 
   function create() {
+    this.dealText = this.add.text(400,300, ['DEAL']).setFontSize(18).setFontFamily('Trebuchet MS').setColor('#000fff').setInteractive();
+    this.dealText.on('pointerdown', () => {this.dealCards(3)})
+    this.dealText.on('pointerover', () => {
+      this.dealText.setColor('#00ffff')
+    });
+    this.dealText.on('pointerout', () => {
+      this.dealText.setColor('#000fff')
+    });
+    this.dealCards = (num) => {
+      for (let i=0; i<num; i++){
+        this.square = this.add.rectangle( 100 +(i * 150), 450, 100, 125, 0x666fff).setInteractive();
+        this.input.setDraggable(this.square)
+      }      
+    }
+
+    this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
+      gameObject.x = dragX;
+      gameObject.y = dragY;
+    } )
 
   }
